1d76a51be7c3d6813f5b02b247126eb99553fde1
erwin <winzon353535@gmail.com>
2022-03-14 11:30:09 -0700

    init add

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  
+  get "/songs" => "songs#index"
+
+  get "/songs/:id" => "songs#show"
+
+  post "/songs" => "songs#create"
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+# Song.create(title: "Godzilla", album: "Music to Be Murdered By", artist: "Eminem", year: 2020)
+# Song.create(title: "Amazing", album: "ROCK PIT", artist: "High and Mighty Color", year: 2007)
+# Song.create(title: "Money for Nothing", album: "Brothers in Arms", artist: "Dire Straits", year: 1985)
